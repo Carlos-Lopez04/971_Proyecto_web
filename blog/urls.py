fa1cc47635fc3b21061f58846080ce0d451fce29
Carlos Lopez <clopez8@uabc.edu.mx>
2026-05-19 18:13:27 -0700

    update information view

--- blog/urls.py
+++ blog/urls.py
@@ -2,10 +2,14 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # Vistas principales
     path('', views.listado_videojuegos, name='listado_videojuegos'),
+    path('detalle/<int:id>/', views.detalle_videojuego, name='detalle_videojuego'),
     path('crear/', views.crear_videojuego, name='crear_videojuego'),
+    path('editar/<int:id>/', views.editar_videojuego, name='editar_videojuego'),
+    path('eliminar/<int:id>/', views.eliminar_videojuego, name='eliminar_videojuego'),
+    
+    # APIs
     path('api/juegos/', views.api_videojuegos, name='api_videojuegos'),
-    path('api/juegos/<int:id>/', views.api_videojuego_detalle, name='api_videojuego_detalle'),
-    path('api/posts/recientes/', views.api_posts_recientes, name='api_posts_recientes'),
-    path('api/portadas/buscar/', views.api_buscar_portadas, name='api_buscar_portadas'),
+    path('api/recientes/', views.api_posts_recientes, name='api_posts_recientes'),
 ]
